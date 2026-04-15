d3b9b9b2974029bc294355a7644d8c34e8785b87
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-04-15 15:17:22 +0300

    Add project files.

---
+++ SFML_01/SFML_01.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML_01");
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+	}
+
+	return 0;
+}
+
