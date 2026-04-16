70707f5273a3b4a498e5829045bdc43c10a4968c
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2026-04-16 22:04:05 +0300

    added imgui and imgui sfml libs

--- SFML_01/SFML_01.cpp
+++ SFML_01/SFML_01.cpp
@@ -1,6 +1,10 @@
+#include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
 #include <cmath>
 #include <optional>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 
 float dot(const sf::Vector2f& a, const sf::Vector2f& b)
@@ -26,27 +30,48 @@ sf::Vector2f normalize(const sf::Vector2f& v)
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML_01");
+	std::fstream fin("config.txt");
+	std::string key;
+	int value;
+
+	unsigned int windowWidth = 800;
+	unsigned int windowHeight = 600;
+
+	while (fin >> key >> value)
+	{
+		if (key == "windowWidth")
+		{
+			windowWidth = value;
+		}
+		if (key == "windowHeight")
+		{
+			windowHeight = value;
+		}
+	}
+
+
+	sf::RenderWindow window(sf::VideoMode({ windowWidth, windowHeight }), "SFML_01");
+	if (!ImGui::SFML::Init(window))
+	{
+		std::cerr << "Failed to initialize ImGui-SFML\n";
+		return 1;
+	}
+
 	sf::Clock clock;
-
-	sf::Vector2u windowSize = window.getSize();
-	float windowWidth = static_cast<float>(windowSize.x);
-	float windowHeight = static_cast<float>(windowSize.y);
-
 
 	sf::CircleShape circle(50.f);
 	circle.setFillColor(sf::Color(100, 250, 50));
 	circle.setOutlineThickness(10.f);
 	circle.setOutlineColor(sf::Color(250, 150, 100));
 	circle.setPosition(sf::Vector2f(windowWidth - 100, windowHeight / 2));
-	circle.setOrigin({50.f, 50.f});
+	circle.setOrigin({ 50.f, 50.f });
 
 	sf::CircleShape circle2(30.f);
 	circle2.setFillColor(sf::Color(150, 150, 150));
 	circle2.setOutlineThickness(10.f);
 	circle2.setOutlineColor(sf::Color(50, 50, 50));
 	circle2.setPosition(sf::Vector2f(100, windowHeight / 2));
-	circle2.setOrigin({30.f, 30.f});
+	circle2.setOrigin({ 30.f, 30.f });
 
 	sf::Vector2f speed(-250.f, -180.f);
 	sf::Vector2f speed2(250.f, 180.f);
@@ -57,6 +82,7 @@ int main()
 	{
 		while (const std::optional event = window.pollEvent())
 		{
+			ImGui::SFML::ProcessEvent(window, *event);
 			if (event->is<sf::Event::Closed>())
 			{
 				window.close();
@@ -65,10 +91,12 @@ int main()
 
 		sf::Time dt = clock.restart(); // get elapsed time
 
+		ImGui::SFML::Update(window, dt);
+
 		circle.move(speed * dt.asSeconds());
 		circle2.move(speed2 * dt.asSeconds());
 
-		
+
 		sf::FloatRect circleBoundingBox = circle.getGlobalBounds();
 		sf::FloatRect circleBoundingBox2 = circle2.getGlobalBounds();
 
@@ -89,7 +117,7 @@ int main()
 		{
 			speed.y = -std::abs(speed.y);
 		}
-	
+
 		// collision with wall for circle 2
 		if (circleBoundingBox2.position.x + circleBoundingBox2.size.x > windowWidth)
 		{
@@ -120,7 +148,7 @@ int main()
 			float velAlongNormal = dot(relativeVelocity, normal);
 
 			// only resolve if moving toward each other
-			if (velAlongNormal)
+			if (velAlongNormal < 0.f)
 			{
 				// Equal mass, elastic collision
 				speed -= velAlongNormal * normal;
@@ -136,13 +164,20 @@ int main()
 			circle2.move(-normal * (overlap * 0.5f));
 		}
 
+		ImGui::Begin("Debug");
+		
+		ImGui::End();
+
 
 		window.clear(sf::Color::Black);
 		window.draw(circle);
 		window.draw(circle2);
+
+		ImGui::SFML::Render(window);
 		window.display();
 	}
 
+	ImGui::SFML::Shutdown();
 	return 0;
 }
 
